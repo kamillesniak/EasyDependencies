914660a5eddcf56c929e294ed75496258bafae33
kamillesniak <kamillesniak02@gmail.com>
2018-12-23 14:05:02 +0100

    Testing flurl

--- EasyDependencies/EasyDependencies/UnitTest1.cs
+++ EasyDependencies/EasyDependencies/UnitTest1.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
 
 namespace EasyDependencies
 {
@@ -9,8 +13,9 @@ namespace EasyDependencies
         [TestMethod]
         public void TestMethod1()
         {
-
-
+            var driver = new FirefoxDriver();
+            var cookies = driver.Manage().Cookies.AllCookies;
+            var url = driver.Url;
         }
     }
 }
